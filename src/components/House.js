fbefcdccbff3e9a65bf2f3ba752f99c1b2b22fa1
tdayetech <150866869+tdayetech@users.noreply.github.com>
2024-03-16 12:48:45 -0500

    removed commented test code

--- src/components/House.js
+++ src/components/House.js
@@ -28,27 +28,7 @@ export const House = (props) => {
     updateHouse(updatedHouse);
   }
 
-  // const editRoom = (roomId) => {
-  //   const updatedHouse = {
-  //     ...house,
-  //     rooms: house.rooms
-  //   };
-  //   updateHouse(updatedHouse);
-  // }
-
   const addNewRoom = (room) => updateHouse({...house, rooms: [...house.rooms, room]});
-
-  // const rooms = () => (
-  //   <ul>
-  //     {house.rooms.map((room, index) => (
-  //       <li key={index}>
-  //         {/* <label>{`${room.name} Area: ${room.area}`}</label> */}
-  //         {roomContent}
-  //         <button onClick={(e) => deleteRoom(room._id)}>Delete</button>
-  //       </li>
-  //     ))}
-  //   </ul>
-  // );
 
   return (
     <div>
@@ -58,7 +38,6 @@ export const House = (props) => {
         onEditRoom={handleEditRoom}
         onDeleteRoom={handleDeleteRoom}
       />
-      {/* {rooms({rooms, houseId: house._id, deleteRoom})} */}
       <NewRoomForm addNewRoom={addNewRoom} />
     </div>
   )
